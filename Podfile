dbff95f7c641bdc8f0c184618450dbb07e1b7224
grivero <grivero@altimetrik.com>
2014-07-03 12:35:09 -0700

    Cocoapods dependencies to install ZKSforce

---
+++ Podfile
@@ -0,0 +1,7 @@
+# Uncomment this line to define a global platform for your project
+target "fitheartSalesforceOauth" do
+platform :ios, '7.1'
+pod 'ZKSforce', '29'
+end
+
+
---
+++ Pods/Pods-fitheartSalesforceOauth-environment.h
@@ -0,0 +1,14 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// ZKSforce
+#define COCOAPODS_POD_AVAILABLE_ZKSforce
+#define COCOAPODS_VERSION_MAJOR_ZKSforce 29
+#define COCOAPODS_VERSION_MINOR_ZKSforce 0
+#define COCOAPODS_VERSION_PATCH_ZKSforce 0
+
