74a54cc77d3396edc9f846b3bd772f526f69dd79
grivero <grivero@altimetrik.com>
2014-07-03 12:36:23 -0700

    ZKSforce controller example - creates participant and his 3 related sections - query to participants

--- fitheartSalesforceOauth/FitHeartViewController.m
+++ fitheartSalesforceOauth/FitHeartViewController.m
@@ -7,23 +7,93 @@
 //
 
 #import "FitHeartViewController.h"
+#import "ZKSforce.h"
 
 @interface FitHeartViewController ()
+@property (weak, nonatomic) IBOutlet UIButton *connectToSalesforceButton;
 
 @end
 
 @implementation FitHeartViewController
 
-- (void)viewDidLoad
-{
+// Salesforce Oauth Info
+static NSString * const client_id     = @"3MVG9sLbBxQYwWqsE4vzTWzE4BS4007eXSUK0Ieb0fGTilipiGg3iklrW0jdjq.WiqEfniuTV2WmkAzwmVvMI";
+static NSString * const client_secret = @"1641254344913232322";
+static NSString * const username      = @"fitheart.mobile.api@fitheart.com.dev";
+static NSString * const password      = @"fitheartmobileapi149z7gMQq5pR5hMp5ythBYwrKEh";
+
+- (void)viewDidLoad{
+    
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning
-{
+- (void)didReceiveMemoryWarning{
+    
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)connectToSalesforceAction:(UIButton *)sender {
+    
+    // alert for user
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Salesforce is connecting"
+                                                    message:@"Please check the Logs in order to see results" delegate:nil cancelButtonTitle:@"Gotcha" otherButtonTitles:nil, nil];
+    [alert show];
+    
+    // hide button
+    self.connectToSalesforceButton.enabled = NO;
+    
+    NSLog(@"Connection to FitHeart Dev Sandbox Started ...");
+    
+    // connection
+    ZKSforceClient *sforce = [[ZKSforceClient alloc] init];
+    [sforce setLoginProtocolAndHost:@"https://test.salesforce.com"];
+    [sforce login:username password:password];
+    
+    NSLog(@"Connection granted and about to create a Participant ...");
+    
+    int randomStudyID = arc4random() % 666000;
+    
+    // object preparation
+    ZKSObject *participant = [ZKSObject withType:@"FitHeart_Participant__c"];
+    [participant setFieldValue:[NSString stringWithFormat:@"%i", randomStudyID] field:@"study_id__c"];
+    
+    // object creation
+    NSArray *results = [sforce create:[NSArray arrayWithObject:participant]];
+    ZKSaveResult *sr = [results objectAtIndex:0];
+    
+    if ([sr success]){
+        
+        NSLog(@"New Participant ID %@", [sr id]);
+        NSLog(@"Now creating Sections for the Participant with ID:%@", [sr id]);
+        
+        ZKSObject *section_fitness = [ZKSObject withType:@"FitHeart_Section__c"];
+        [section_fitness setFieldValue:[NSString stringWithFormat:@"%@", [sr id]] field:@"Participant__c"];
+        [section_fitness setFieldValue:@"1" field:@"section_id__c"];
+        results = [sforce create:[NSArray arrayWithObject:section_fitness]];
+        
+        ZKSObject *section_health = [ZKSObject withType:@"FitHeart_Section__c"];
+        [section_health setFieldValue:[NSString stringWithFormat:@"%@", [sr id]] field:@"Participant__c"];
+        [section_health setFieldValue:@"2" field:@"section_id__c"];
+        results = [sforce create:[NSArray arrayWithObject:section_health]];
+        
+        ZKSObject *section_mood = [ZKSObject withType:@"FitHeart_Section__c"];
+        [section_mood setFieldValue:[NSString stringWithFormat:@"%@", [sr id]] field:@"Participant__c"];
+        [section_mood setFieldValue:@"3" field:@"section_id__c"];
+        results = [sforce create:[NSArray arrayWithObject:section_mood]];
+        
+        NSLog(@"Sections created for the Participant!");
+        
+        NSLog(@"Query all Participants and show result");
+        ZKQueryResult *qr = [sforce query:@"SELECT Id, study_id__c FROM FitHeart_Participant__c"];
+        for (ZKSObject *o in [qr records]) {
+            NSLog(@"%@", [o fieldValue:@"study_id__c"]);
+        }
+        
+    }else
+        NSLog(@"error creating participant %@ %@", [sr statusCode], [sr message]);
+    
 }
 
 @end
